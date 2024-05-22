c2860d3266aa1233787400b163d527fdc7dafe61
hozan23 <hozan23@karyontech.net>
2024-05-22 15:33:58 +0200

    jsonrpc: cargo fmt

--- jsonrpc/src/lib.rs
+++ jsonrpc/src/lib.rs
@@ -12,7 +12,7 @@ pub use server::{
     channel::{ArcChannel, Channel, Subscription, SubscriptionID},
     pubsub_service::{PubSubRPCMethod, PubSubRPCService},
     service::{RPCMethod, RPCService},
-    Server, ServerBuilder
+    Server, ServerBuilder,
 };
 
 pub use karyon_jsonrpc_macro::{rpc_impl, rpc_pubsub_impl};
