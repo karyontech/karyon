778b4fcb8e16cf47e86b86eabaf643215dadc6e2
hozan23 <hozan23@karyontech.net>
2025-05-15 10:06:40 +0200

    Upgrade bincode to 2.0.1

--- core/src/util/decode.rs
+++ core/src/util/decode.rs
@@ -4,7 +4,7 @@ use crate::Result;
 
 /// Decodes a given type `T` from the given slice. returns the decoded value
 /// along with the number of bytes read.
-pub fn decode<T: Decode>(src: &[u8]) -> Result<(T, usize)> {
+pub fn decode<T: Decode<()>>(src: &[u8]) -> Result<(T, usize)> {
     let (result, bytes_read) = bincode::decode_from_slice(src, bincode::config::standard())?;
     Ok((result, bytes_read))
 }
