ff4ac639de3322ddbcbaf946afd0c4caabb77861
hozan23 <hozan23@users.noreply.github.com>
2025-07-25 08:42:57 +0000

    deploy: 6fe4948abdbde9d1e216b3b8c8bf4ce3fd12841a

---
+++ karyon_eventemitter/eventemitter/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["DEFAULT_CHANNEL_BUFFER_SIZE"],"fn":["random_id"],"struct":["Event","EventEmitter","EventListener"],"trait":["AsEventTopic","AsEventValue"],"type":["EventListenerID","Listeners"]};
